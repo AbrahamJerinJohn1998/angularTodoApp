9e68d847857f16fd796f5d7a8bebb97bcf16bf49
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-25 12:45:42 +0530

    View todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
-
+import { ViewTodoComponent } from './view-todo/view-todo.component';
+import { RouterModule, Routes } from '@angular/router';
+const myRoute:Routes=[
+  {
+    path:"",
+    component:AddTodoComponent
+  },
+  {
+    path:"/viewtodo",
+    component:ViewTodoComponent
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    ViewTodoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
